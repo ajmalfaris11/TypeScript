b916adcf509183218772be70702515d2c7825819
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-04-01 12:10:00 +0530

    timing to use any

---
+++ intro/any.js
@@ -0,0 +1,9 @@
+"use strict";
+//  :any 
+Object.defineProperty(exports, "__esModule", { value: true });
+//  its not good practice to use any keyword
+var hero; // in this case better to use :variableType other ways it will take all the type
+function getHero() {
+    return "thro";
+}
+hero = getHero();
---
+++ intro/any.ts
@@ -0,0 +1,17 @@
+//  :any 
+
+//  its not good practice to use any keyword
+
+let hero; // in this case better to use :variableType other ways it will take all the type
+
+function getHero(){
+    return "thro"
+}
+
+hero = getHero();
+
+let name:string; // this time we can only assign string variable to name
+
+
+
+export {}
